70cbc75e1b531b0703e3656c82b02404c0e71902
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2024-02-01 21:30:48 +0100

    Add c# replacement for runway.gd

---
+++ src/scripts/runway/Runway.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public class Runway : Node2D
+{
+    private RunwayTimer _timer;
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _timer = GetNode<RunwayTimer>("./RunTimer/Timer");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
