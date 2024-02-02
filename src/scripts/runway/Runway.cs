8d34a71e22cef64b6deca6ae9f073fb698cdb055
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2024-02-02 16:43:29 +0100

    Save total time to playerdata

--- src/scripts/runway/Runway.cs
+++ src/scripts/runway/Runway.cs
@@ -4,6 +4,7 @@ using System;
 public class Runway : Node
 {
     private Timer _timer;
+    private Node _playerData;
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -11,13 +12,22 @@ public class Runway : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _playerData = GetNode("/root/player_data");
         _timer = GetNode<Timer>("HUD_TotalTime");
+        _timer.Connect("Ticked", this, nameof(OnTick));
         _timer.Start();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void OnTick(int minutes, int seconds)
+    {
+        _playerData.Set("minutes", minutes);
+        _playerData.Set("seconds", seconds);
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+
 }
