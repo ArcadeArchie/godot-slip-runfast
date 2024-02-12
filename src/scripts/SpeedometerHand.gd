c5391a0a9115ad60cfaaf4d1a48ca36942f470b1
null_man <96726090+the-nullman@users.noreply.github.com>
2024-02-12 14:53:59 +0100

    fixing stuff

--- src/scripts/SpeedometerHand.gd
+++ src/scripts/SpeedometerHand.gd
@@ -4,13 +4,12 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+var rotation_rad
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	rotation_rad = 0
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate(0.1)
+	rotation_rad = rotation
